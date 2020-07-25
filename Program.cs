58cb007864b2cf140552f2a49c778d7213034a30
FinchyG <lagaunt120@yahoo.com>
2020-07-25 08:06:19 +0100

    + application

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookshelfSorter
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserData ud = new UserData();
+
+            while (true)
+            {
+                Console.WriteLine("Press any key to add a book to your bookshelf or 'v' to view your bookshelf:");
+                ud.Choice = Console.ReadLine().ToLower();
+
+                if (ud.Choice == "v")
+                    ud.DisplayBookshelf();
+                
+                else
+                    ud.AddBook();
+            }
+        }
+    }
+}
---
+++ UserData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookshelfSorter
+{
+    class UserData
+    {
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+
+        public string Choice { get; set; }
+
+        public SortedDictionary<string, string> Bookshelf = new SortedDictionary<string, string>();
+
+        public void DisplayBookshelf()
+        {
+            foreach (KeyValuePair<string, string> book in Bookshelf)
+            {
+                Console.WriteLine("{0}, by {1}", book.Key, book.Value);
+            }
+        }
+
+        public void AddBook()
+        {
+            Console.WriteLine("What's the book's title?");
+            BookTitle = Console.ReadLine();
+
+            Console.WriteLine("Who's the book's author?");
+            BookAuthor = Console.ReadLine();
+
+            Bookshelf.Add(BookTitle, BookAuthor);
+        }
+    }
+}
